6faabc63e0d0f3d78537518f8776cf3859de97d7
ljw1412 <506644791@qq.com>
2019-09-21 13:56:20 +0800

    修复自定义替换和跳过日语文本中的汉字简化

--- executor.js
+++ executor.js
@@ -3,11 +3,14 @@ const { tify, sify } = require('chinese-conv')
 
 async function execute(path, rp = {}) {
   let context = await utils.readFile(path)
+  // 自定义替换文本
   if (typeof rp === 'object') {
     Object.keys(rp).forEach(key => {
-      context = context.replace(key, rp[key])
+      regx = new RegExp(key, 'g')
+      context = context.replace(regx, rp[key])
     })
   }
+  // 文件解析
   const typeList = ['[Script Info]', '[V4 Styles]', '[Events]']
   const indexList = typeList
     .map(item => context.indexOf(item))
@@ -21,6 +24,7 @@ async function execute(path, rp = {}) {
       if (text.includes(item)) subtitles[item] = text
     })
   }
+  // 找到字幕块
   if (subtitles['[Events]']) {
     const events = subtitles['[Events]'].split('\r\n')
     const format = events[1]
@@ -39,10 +43,15 @@ async function execute(path, rp = {}) {
       // event.length = textIndex + 1
 
       if (event.length - 1 >= textIndex) {
+        // 原始字符串
         const oText = event[textIndex]
         console.log(oText)
+        // 排除日语文本的简体化
+        if (/[\u3040-\u31ff]/g.test(oText)) continue
+        // 简体化后
         const sText = sify(oText)
         console.log(sText)
+        // 替换原始字符串
         context = context.replace(oText, sText)
       }
     }
